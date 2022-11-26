027e4537ae69148a256d8a0b6480cb52e539f149
IBugagashI <popykin.alek@yandex.ru>
2022-11-26 21:08:49 +0300

    ДЗ 2, доп задача

---
+++ DZ2/DopTask/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int x;
+int i = 0;
+int z = 0;
+do
+{
+    x = int.Parse(Console.ReadLine());
+    if (x > i)
+    z = i;
+    i = x;
+} 
+while (x != 0);
+Console.WriteLine($"Второе максимальное число: {z}");
