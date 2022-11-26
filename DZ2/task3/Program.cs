08507ea2403ca447637c1a6f569a4fd2af9c87d9
IBugagashI <popykin.alek@yandex.ru>
2022-11-26 20:33:51 +0300

    ДЗ 2, задача 3

---
+++ DZ2/task3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+if (x >= 1 && x <= 5)//(1 <= x <= 5) 
+{
+Console.WriteLine("Будний");
+}
+else if (x == 6 || x == 7)
+{
+Console.WriteLine("Выходной");
+}
+else
+{
+Console.WriteLine("Нет таких");
+} 
+
+/* Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+if (x == 1 || x == 2 || x == 3 || x == 4 || x == 5)//(1 <= x <= 5) 
+{
+Console.WriteLine("Будний");
+}
+else if (x == 6 || x == 7)
+{
+Console.WriteLine("Выходной");
+}
+else
+{
+Console.WriteLine("Нет таких");
+} */
