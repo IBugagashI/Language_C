c164de9a57f443ae8bec77da975aa0ab1b1fd067
IBugagashI <popykin.alek@yandex.ru>
2022-11-26 19:47:25 +0300

    ДЗ 2, задача 2

---
+++ DZ2/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+if (x < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+    return;
+}
+while (x > 999)
+{
+    x = x / 10;
+}
+Console.Write(x % 10);
