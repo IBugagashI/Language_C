f12399836cff7928e20209de5e85fe274baa9d04
IBugagashI <popykin.alek@yandex.ru>
2022-11-26 18:49:41 +0300

    ДЗ 2, задача 1

---
+++ DZ2/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+while(true)
+{
+    Console.Write("Введите трёхзначное число: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x < 100 | x > 999)
+    {
+        Console.WriteLine("Ц, ц, ц !!! Нужно трёхзначное число");
+    }
+    else
+    {
+        Console.Write((x / 10) % 10);
+        break;
+    }
+}
+// как "бороться" с буквами, я не соображу
