8d2d5ae1d179535fb8944d6f5d189773a8b9a671
IBugagashI <popykin.alek@yandex.ru>
2022-11-28 19:33:27 +0300

    ДЗ 3, 1 задача

---
+++ DZ3/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+while (true)
+{
+    int x = int.Parse(Console.ReadLine());
+    if (x < 10000)
+    {
+        Console.WriteLine("!!!Нужно пятизначное число!!!");
+        Console.Write("Введите пятизначное число: ");
+    }
+    else
+    {
+        if ((x / 10000) == (x % 10) & ((x / 1000) % 10 == (x / 10) % 10))
+        Console.Write("Да");
+        else
+        Console.Write("Нет");
+        break;
+    }
+}
