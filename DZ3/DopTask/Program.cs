4240281e680f894c43b17b7b59e60a20b9e76ef9
IBugagashI <popykin.alek@yandex.ru>
2022-11-28 21:08:16 +0300

    ДЗ 3, доп. задача - начало положено

---
+++ DZ3/DopTask/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+void FillArray(int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < N)
+    {
+        collection[index] = index + 1;
+        index++;
+    }
+}
+void PrintArray(int [] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] bushes = new int[N];
+
+FillArray(bushes);
+PrintArray(bushes);
