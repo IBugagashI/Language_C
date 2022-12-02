a1aa58c4d78a3e1aa2bc8bc592fdf160cd282ef1
IBugagashI <popykin.alek@yandex.ru>
2022-12-02 15:22:02 +0300

    ДЗ 4, решение второй задачи задачи

---
+++ DZ4/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int victim = number;
+int sum = 0;
+//for(int i = 0; number != 0; i++)
+while (victim > 0)
+{
+    sum += victim % 10;
+    victim /= 10;
+}
+Console.WriteLine($"Суммой чисел, числа {number}, является {sum}");
