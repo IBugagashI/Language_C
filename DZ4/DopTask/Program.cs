e8bede3ec694acc2864f820033e07326eac72945
IBugagashI <popykin.alek@yandex.ru>
2022-12-02 18:26:09 +0300

    ДЗ 4, доп задача - неудная попытка

--- DZ4/DopTask/Program.cs
+++ DZ4/DopTask/Program.cs
@@ -4,7 +4,7 @@ Console.Clear();
 Console.Write("Введите количество чисел последовательности: ");
 int N = int.Parse(Console.ReadLine());
 int[] array = new int[N];
-int[] revers = new int[N];
+int[] last = new int[N];
 for (int i = 0; i < array.Length; i++)
 {
     Console.Write($"Введите {i + 1} число последовательности: ");
@@ -19,17 +19,26 @@ if (K > N)
 }
 if (K > 0)
 {
-    
-
+    static void ShiftElements(int[] arr)
+        {
+            int last = arr[arr.Length - 1];
+ 
+            for (int i = arr.Length - 2; i >= 0; i--)
+            {
+                arr[i + 1] = arr[i];
+            }
+ 
+            arr[0] = last;
+        }    
     var str = string.Join(", ", array);
-    Console.WriteLine($"Последовательность со сдвигом: {str}");
+    Console.WriteLine($"Последовательность со сдвигом вправо: {str}");
 }
 else
 {
 
     
     var str = string.Join(", ", array);
-    Console.WriteLine($"Последовательность со сдвигом: {str}");
+    Console.WriteLine($"Последовательность со сдвигом влево: {str}");
 }
 /* array.Reverse(array, K);               // 21|345
 array.Reverse(array + K, N - K); // 21|543
