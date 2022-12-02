4bded9585707c45ea4ea36528dd3eabcea2308d6
IBugagashI <popykin.alek@yandex.ru>
2022-12-02 15:54:26 +0300

    ДЗ 4, решение третьей задачи задачи

---
+++ DZ4/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов, предоставить возможность пользователю ввести числа и вывести их на экран.
+Console.Clear();
+Console.WriteLine("Зачем оно вам надо, не ведаю, но сделаем массив из 8 элементов!");
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите {i + 1} число массива: ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+var newArray = string.Join(", ", array);
+Console.WriteLine($"Числа вашего массива: {newArray}.");
