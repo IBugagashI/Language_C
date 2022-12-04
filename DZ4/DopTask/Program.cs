8e30cbd9ae565989825db589e3627ba7b374b69f
IBugagashI <popykin.alek@yandex.ru>
2022-12-04 23:49:40 +0300

    ДЗ 4, доп задача - решение для смещения вправо +

--- DZ4/DopTask/Program.cs
+++ DZ4/DopTask/Program.cs
@@ -5,7 +5,7 @@ Console.Write("Введите количество чисел последова
 int N = int.Parse(Console.ReadLine());
 int[] array = new int[N];
 int[] offsetArray = new int[N];
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < N; i++)
 {
     Console.Write($"Введите {i + 1} число последовательности: ");
     array[i] = int.Parse(Console.ReadLine());
@@ -14,23 +14,37 @@ Console.Write("Введите число, на значение которого
 int K = int.Parse(Console.ReadLine());
 if (K > N)
 {
-    Console.Write("!!!Ваше число превышает колличество элементов последовательности!!! \n Введите число заново: ");
+    Console.Write("!!!Ваше число превышает колличество элементов последовательности!!!\nВведите число заново: ");
+    K = int.Parse(Console.ReadLine());
+}
+if (K == 0)
+{
+    Console.Write("!!! Укажите число отличное от 0 !!!\nВведите число заново: ");
     K = int.Parse(Console.ReadLine());
 }
 if (K > 0)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        offsetArray[i] = (i + K) % array.Length;
+        if (i + K >= array.Length)
+        {
+            offsetArray[i + K - array.Length] = array[i];
+        }
+        else
+        {
+            offsetArray[i + K] = array[i];
+        }
     }   
     var str = string.Join(", ", offsetArray);
     Console.WriteLine($"Последовательность со сдвигом вправо: {str}");
 }
 else
 {
-
-    
-    var str = string.Join(", ", array);
+    for (int j = array.Length - 1; j <= 0; j--)
+    {
+        offsetArray[j] = array[j];
+    }
+    var str = string.Join(", ", offsetArray);
     Console.WriteLine($"Последовательность со сдвигом влево: {str}");
 }
 /* array.Reverse(array, K);               // 21|345
