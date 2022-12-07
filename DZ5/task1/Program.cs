47cbdcbba5273fc2ebc3f2cf9ca7a6e887ff59c9
IBugagashI <popykin.alek@yandex.ru>
2022-12-07 18:26:05 +0300

    ДЗ 5, решение 2й задачи

---
+++ DZ5/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Clear();
+Console.Write("Введите количество чисел массива: ");
+int N = int.Parse(Console.ReadLine());
+while (N < 1)
+{
+    Console.Write("!!!Нужно число больше нуля!!!\nВведите число заново: ");
+    N = int.Parse(Console.ReadLine());
+}
+int[] array = new int[N];
+int even = 0;
+int index = 0;
+
+while (index < array.Length)
+{
+    array[index] = new Random().Next(100, 999);
+    index++;
+}
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    even++;
+}
+Console.WriteLine($"Чисел в массиве: {array.Length}\nЧисла массива: {string.Join(", ", array)}\nЧётных чисел в массиве: {even}");
---
+++ DZ5/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 10);
+}
+void ReleaseArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum += array[i];
+    }
+    Console.WriteLine($"Сумма нечётных позиций массива: {sum}");
+}
+Console.Clear();
+Console.Write("Введите количество чисел массива: ");
+int N = int.Parse(Console.ReadLine());
+int[] array = new int[N];
+InputArray(array);
+Console.WriteLine($"Числа массива: {string.Join(", ", array)}");
+ReleaseArray(array);
