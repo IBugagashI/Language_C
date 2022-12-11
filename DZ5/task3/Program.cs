e483247af1c56b2573ef75cb4f118f451a8dcd02
IBugagashI <popykin.alek@yandex.ru>
2022-12-11 18:07:07 +0300

    ДЗ 5, решение 3й задачи, поправка решения

--- DZ5/task3/Program.cs
+++ DZ5/task3/Program.cs
@@ -1,8 +1,9 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 void InputArray(double [] array)
 {
+    Random rand = new Random();
     for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(-10, 10);
+        array[i] = rand.NextDouble() + new Random().Next(-10, 10);
 }
 void ReleaseArray(double [] array)
 {
@@ -21,11 +22,11 @@ void ReleaseArray(double [] array)
     if(min < 0 && max > 0)
     {
         min *= -1;
-        Console.WriteLine($"Наибольшее число массива: {max}\nНаименьшее число массива: -{min}\nРазница: {max - min}");
+        Console.WriteLine($"Наибольшее число массива: {max:F2}\nНаименьшее число массива: -{min:F2}\nРазница: {max - min:F2}");
     }
     else
     {
-        Console.WriteLine($"Наибольшее число массива: {max}\nНаименьшее число массива: {min}\nРазница: {max - min}");
+        Console.WriteLine($"Наибольшее число массива: {max:F2}\nНаименьшее число массива: {min:F2}\nРазница: {max - min:F2}");
     }
 }
 Console.Clear();
@@ -33,5 +34,5 @@ Console.Write("Введите количество чисел массива: ")
 int N = Convert.ToInt32(Console.ReadLine());
 double[] array = new double [N];
 InputArray(array);
-Console.WriteLine($"Числа массива: {string.Join(", ", array)}");
+Console.WriteLine($"Числа массива: [{string.Join(" | ", array)}]");
 ReleaseArray(array);
