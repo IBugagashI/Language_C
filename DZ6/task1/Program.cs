a0dde8e4950311c69947db3a8c076812953fa19d
IBugagashI <popykin.alek@yandex.ru>
2022-12-15 01:27:19 +0300

    дз 6, решение 1й задачи

--- DZ6/task1/Program.cs
+++ DZ6/task1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        count++;
+    }
+    Console.WriteLine($"Количество чисел больше 0: {count}");
+} 
+Console.Clear();
+Console.Write("Введите количество чисел для ввода: ");
+int M = Convert.ToInt32(Console.ReadLine());
+while (M < 1)
+{
+    Console.Write("!!!Укажите чило больше 0!!! ");
+    Console.Write("Введите число заново: ");
+    M = Convert.ToInt32(Console.ReadLine());
+} 
+int[] array = new int[M];
+InputArray(array);
+Console.WriteLine($"Числа массива: [{string.Join(", ", array)}]");
+ReleaseArray(array);
